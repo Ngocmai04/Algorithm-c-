13b84037583d38c1fb90f3113938ad5a285247fb
Ngocmai04 <truongngocmai3@gmail.com>
2024-10-12 08:00:23 +0700

    update

--- maze22.cpp
+++ maze22.cpp
@@ -38,17 +38,19 @@ int solve(){
 }
 
 int main() {
-	int n,m, xp1,xp2;
-   cin >> n >> m;
-    vector<vector<int>> v(n, vector<int>(m));
+	
+   cin >> n >> m>>r>>c;
+    vector<vector<int>> v(n+1, vector<int>(m+1));
     for (int i=0;i<n;i++){
     	for (int j=0;j<m;j++){
     		cin>>v[i][j];
+    		a[i][j] = v[i][j];
 		}
 	}
-    
+    int result=solve();
 
-    cout << solve() << endl;
+    cout << result << endl;
+}
 
 
 
